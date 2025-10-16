1dacce696d27093baf6e1fde8a41eff359e25282
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 12:12:51 +0530

    Update exp13.c

--- exp13.c
+++ exp13.c
@@ -1,5 +1,6 @@
 // To simulate file allocation strategies: (a) Sequential, (b) Indexed, (c) Linked
 
+// program (a)
 #include <iostream>
 #include <vector>
 using namespace std; 
@@ -33,3 +34,87 @@ cout << "Enter length of file " << i+1 << ": ";
 			cout << "File " << i+1 << " cannot be allocated." << endl; 
 		} 
 }
+
+// program (b)
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+int main() { 
+	int n, f, indexBlock, length; 
+	cout << "Enter total number of disk blocks: "; 
+	cin >> n; 
+	vector<int> block(n, 0);
+cout << "Enter number of files: ";
+cin >> f; 
+for (int i = 0; i < f; i++) {
+cout << "\nEnter index block for file " << i+1 << ": ";
+cin >> indexBlock; 
+if (block[indexBlock] == 1) { 
+	cout << "Index block already allocated. File cannot be stored.\n";
+continue;
+} 
+block[indexBlock] = 1;
+cout << "Enter number of blocks needed for file " << i+1 << ": ";
+cin >> length; 
+vector<int> allocated; 
+cout << "Enter block numbers: "; 
+for (int j = 0; j < length; j++) { 
+	int b; 
+	cin >> b; 
+	if (b < n && block[b] == 0) { 
+		block[b] = 1; 
+			allocated.push_back(b); 
+	} else { 
+			cout << "Block " << b << " already allocated or invalid.\n";
+} 
+		} 
+		cout << "File " << i+1 << " stored using index block " 
+				<< indexBlock << " -> "; 
+		for (int b : allocated) cout << b << " "; 
+		cout << endl; 
+	} 
+	return 0; 
+} 
+
+// program (c)
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+struct Node { 
+	int block; 
+	Node* next; 
+}; 
+int main() { 
+	int n, f, length; 
+	cout << "Enter total number of disk blocks: "; 
+	cin >> n; 
+	vector<int> block(n, 0);
+cout << "Enter number of files: ";
+cin >> f; 
+for (int i = 0; i < f; i++) {
+cout << "\nEnter number of blocks required for file " << i+1 << ": ";
+cin >> length; 
+Node* head = nullptr; 
+Node* temp = nullptr; 
+cout << "Enter block numbers: "; 
+for (int j = 0; j < length; j++) { 
+	int b; 
+	cin >> b; 
+	if (b < n && block[b] == 0) { 		block[b] = 1;
+Node* newNode = new Node{b, nullptr}; 
+			if (!head) head = newNode; 
+			else temp->next = newNode; 
+			temp = newNode; 
+	} else { 
+			cout << "Block " << b << " already allocated or invalid.\n"; 
+	} 
+} 
+cout << "File " << i+1 << " allocated blocks (linked): "; 
+temp = head; 
+while (temp) { 
+	cout << temp->block << " -> ";
+			temp = temp->next; 		} 
+		cout << "NULL" << endl; 	} 
+	return 0; }
