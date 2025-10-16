18016a13c013540c3f93c7e44483554ca814691c
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:43:57 +0530

    Create exp1.c

---
+++ exp1.c
@@ -0,0 +1,16 @@
+// To demonstrate the creation of a child process using the fork() system call in Linux. 
+
+#include <stdio.h>
+#include <unistd.h>
+int main() { 
+int pid; 
+pid = fork(); 
+if (pid < 0) {
+printf("Fork failed!\n");
+} else if (pid == 0) {
+printf("This is the child process. PID = %d\n", getpid());
+} else {
+printf("This is the parent process. PID = %d, Child PID = %d\n", getpid(), pid);
+}
+return 0;
+}
