7ce436de7ea0e98ac428c0e65334b93e004f8258
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 12:01:21 +0530

    Create exp8.c

---
+++ exp8.c
@@ -0,0 +1,15 @@
+// To create a thread using the pthreads library in C and run its function
+
+#include <stdio.h>
+#include <pthread.h> 
+void* threadFunc(void* arg) {
+printf("Hello from thread!\n");
+return NULL;
+} 
+int main() {
+pthread_t tid; 
+pthread_create(&tid, NULL, threadFunc, NULL); 
+pthread_join(tid, NULL); 
+printf("Thread execution completed.\n");
+return 0;
+}
